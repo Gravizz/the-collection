b53a1270ce4279bd7560a9496dcc3587872dc7f2
Gravizz <peeke0rr@gmail.com>
2024-08-29 21:07:24 +0700

    feat:add draft footer & navbar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,38 @@
+import Link from 'next/link';
 import React from 'react';
 
-function Footer() {
-  return <div>Footer</div>;
-}
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-blue-800 text-white py-6 px-4 text-center w-full">
+      <div className="flex flex-col md:flex-row justify-between items-center space-y-6 md:space-y-0">
+        {/* Left Side */}
+        <p className="mb-4 md:mb-0">
+          © {new Date().getFullYear()} by Preechapong Khanmajan.
+        </p>
+
+        {/* Right Side */}
+        <div className="flex flex-col md:flex-row items-start space-y-6 md:space-y-0 md:space-x-8">
+          <div className="flex flex-col items-center space-y-1">
+            <span className="font-bold">Call</span>
+            <span>+66 95-616-3515</span>
+          </div>
+          <div className="flex flex-col items-center space-y-1">
+            <span className="font-bold">Write</span>
+            <span>peeke0rr@gmail.com</span>
+          </div>
+          <div className="flex flex-col items-center space-y-1">
+            <span className="font-bold">Follow</span>
+            <div className="flex space-x-3">
+              <Link href="https://facebook.com">Facebook</Link>
+              <Link href="https://linkedin.com">LinkedIn</Link>
+              <Link href="https://instagram.com">Instagram</Link>
+              <Link href="https://github.com">GitHub</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,28 @@
+import Link from 'next/link';
 import React from 'react';
 
-function Navbar() {
-  return <div>Navbar</div>;
-}
+const Navbar: React.FC = () => {
+  return (
+    <nav className="w-full md:w-64 bg-blue-700 text-white p-5">
+      <div className="h-full pt-10 flex flex-col items-center space-y-10">
+        <Link href="/" passHref>
+          About me
+        </Link>
+
+        <Link href="/resume" passHref>
+          Resume
+        </Link>
+
+        <Link href="/projects" passHref>
+          Project
+        </Link>
+
+        <Link href="/contact" passHref>
+          Contact
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
